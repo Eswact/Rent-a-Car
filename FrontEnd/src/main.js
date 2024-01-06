10da1749a3f3011eb1a484486b85be325abb3845
Eren Sarıbay <eren99srb@gmail.com>
2024-01-06 22:40:13 +0300

    delete banner

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -7,8 +7,8 @@ import router from './router'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCar, faHome, faCircleInfo, faHeadset, faBars, faXmark, faUsers, faSuitcaseRolling, faGasPump, faGaugeSimpleHigh, faCalendarDays, faCreditCard, faCircleChevronRight, faPlus, faCircleXmark} from '@fortawesome/free-solid-svg-icons'
-library.add(faCar, faHome, faCircleInfo, faHeadset, faBars, faXmark, faUsers, faSuitcaseRolling, faGasPump, faGaugeSimpleHigh, faCalendarDays, faCreditCard, faCircleChevronRight, faPlus, faCircleXmark)
+import { faCar, faHome, faCircleInfo, faHeadset, faBars, faXmark, faUsers, faSuitcaseRolling, faGasPump, faGaugeSimpleHigh, faCalendarDays, faCreditCard, faCircleChevronRight, faPlus, faCircleXmark, faTrashCan} from '@fortawesome/free-solid-svg-icons'
+library.add(faCar, faHome, faCircleInfo, faHeadset, faBars, faXmark, faUsers, faSuitcaseRolling, faGasPump, faGaugeSimpleHigh, faCalendarDays, faCreditCard, faCircleChevronRight, faPlus, faCircleXmark, faTrashCan)
 
 const app = createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
--- FrontEnd/src/views/AdminView.vue
+++ FrontEnd/src/views/AdminView.vue
@@ -29,8 +29,8 @@
         const file = document.getElementById('fileInput').files[0];
         postFormData('admin/addBannerImage', {
             file: file
-        }, function (data) {
-            console.log(data);
+        }, function () {
+            console.log('Başarılı');
         }, function (error) {
             console.error('Bir hata oluştu:', error);
         });
@@ -39,10 +39,23 @@
             alt: file.name,
             title: document.getElementById('bannerHeader').value,
             description: document.getElementById('bannerDescription').value
-        }, function (data) {
-            console.log(data);
+        }, function () {
+            console.log('Başarılı');
         }, function (error) {
             console.error('Bir hata oluştu:', error);
+        }).then(() => {
+            getBanners();
+        });
+        document.querySelector('.bannerModal').classList.remove('open');
+    };
+
+    const deleteBanner = (bannerId) => {
+        postData(`home/banners/delete/${bannerId}`, {}, function () {
+            console.log('Başarılı');
+        }, function (error) {
+            console.error('Bir hata oluştu:', error);
+        }).then(() => {
+            getBanners();
         });
     };
 
@@ -50,7 +63,6 @@
         if (!file) {
             return;
         }
-
         const reader = new FileReader();
         reader.onload = (e) => {
             selectedImage.value = e.target.result;
@@ -66,43 +78,79 @@
         document.querySelector('.bannerModal').classList.remove('open');
     };
 
-    onMounted(() => {
+    const getBanners = () => {
         fetchData('home/banners',
             function (data) {
                 banners.value = data;
+                console.log(banners.value);
             },
             function (error) {
                 console.error('Bir hata oluştu:', error);
             }
         );
+    };
+
+    onMounted(() => {
+        getBanners();
     });
 </script>
 
 <template>
-    <div>
-        <h1>Bannerlar</h1>
-        <hr class="mb-[10px]">
-        <div class="flex items-center gap-[10px] flex-wrap">
-            <div v-for="banner in banners" :key="banner.bannerId" class="w-[360px] h-[120px] rounded-[10px] overflow-hidden">
-                <img :src="getBannerImage(banner.src)" alt="banner" class="w-full h-full object-cover">
-            </div>
-            <div @click="openBannerModal" class="w-[360px] h-[120px] rounded-[10px] overflow-hidden border-[1px] border-main bg-main-shadow flex justify-center items-center cursor-pointer">
-                <div class="border-main border-[2px] rounded-[50%] text-main bg-white w-[50px] h-[50px] shadow-sm shadow-main flex justify-center items-center">
-                    <font-awesome-icon icon="fa-solid fa-plus" size="2xl"/>
+    <div class="w-full flex flex-col gap-[20px] py-[20px]">
+        <div class="w-full">
+            <h1 class="text-[20px] text-main">Afişler</h1>
+            <hr class="mb-[10px]">
+            <div class="flex items-center justify-center gap-[10px] flex-wrap p-[10px]">
+                <div v-for="banner in banners" :key="banner.bannerId" class="bannerCard w-[360px] h-[120px] rounded-[10px] overflow-hidden cursor-pointer relative">
+                    <img :src="getBannerImage(banner.src)" alt="banner" class="w-full h-full object-cover">
+                    <button @click="deleteBanner(banner.bannerId)" class="deleteBanner absolute top-[4px] right-[4px] py-[1px] px-[8px] text-white bg-second rounded-[10px]"><font-awesome-icon :icon="['fas', 'trash-can']"/></button>
+                </div>
+                <div @click="openBannerModal" class="w-[360px] h-[120px] rounded-[10px] overflow-hidden border-[1px] border-main bg-main-shadow flex justify-center items-center cursor-pointer">
+                    <div class="border-main border-[2px] rounded-[50%] text-main bg-white w-[50px] h-[50px] shadow-sm shadow-main flex justify-center items-center">
+                        <font-awesome-icon icon="fa-solid fa-plus" size="2xl"/>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="w-full flex flex-col gap-[20px] py-[20px]">
+        <div class="w-full">
+            <h1 class="text-[20px] text-main">Markalar</h1>
+            <hr class="mb-[10px]">
+            <div class="flex items-center justify-center gap-[10px] flex-wrap p-[10px]">
+                
+            </div>
+        </div>
+    </div>
+    <div class="w-full flex flex-col gap-[20px] py-[20px]">
+        <div class="w-full">
+            <h1 class="text-[20px] text-main">Arabalar</h1>
+            <hr class="mb-[10px]">
+            <div class="flex items-center justify-center gap-[10px] flex-wrap p-[10px]">
+                
+            </div>
+        </div>
+    </div>
+    <div class="w-full flex flex-col gap-[20px] py-[20px]">
+        <div class="w-full">
+            <h1 class="text-[20px] text-main">Şirket</h1>
+            <hr class="mb-[10px]">
+            <div class="flex items-center justify-center gap-[10px] flex-wrap p-[10px]">
+                
+            </div>
+        </div>
+    </div>
 
-    <div class="bannerModal flex absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.8)] justify-center items-center">
-        <div class="bg-white w-[800px] rounded-[10px] relative border-[1px] border-main-shadow shadow-lg shadow-main-shadow">
+    <!-- Modals -->
+    <div class="bannerModal flex fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.8)] justify-center items-center">
+        <div class="bg-white w-[800px] rounded-[10px] relative border-[1px] border-main-shadow shadow-lg shadow-main-shadow max-w-[95%]">
             <div class="justify-between items-center px-[16px] py-[10px] border-b-[1px] shadow-md border-main-shadow">
                 <h1 class="text-[20px] text-main font-[600]">Banner Ekle</h1>
                 <button class="absolute top-[-11px] right-[-13px] text-second" @click="closeBannerModal"><font-awesome-icon :icon="['fas', 'circle-xmark']" size="2xl" class="bg-white rounded-[50%] shadow-lg shadow-second-shadow"/></button>
             </div>
             <form id="modalContent" class="flex flex-col gap-[10px] p-[10px]" enctype="multipart/form-data" method="POST">
                 <div class="relative">
-                    <input type="file" id="fileInput" ref="fileInput" @change="handleFileChange" class="hidden" accept=".jpg, .jpeg, .png, .webp">
+                    <input type="file" id="fileInput" ref="fileInput" @change="handleFileChange" class="hidden" accept=".jpg, .jpeg, .png, .webp" required>
                     <div class="w-full h-[240px] border-[1px] rounded-[6px] flex justify-center items-center cursor-pointer" @click="triggerFileInput" @dragover.prevent="handleDragOver" @drop.prevent="handleDrop">
                         <div v-if="!selectedImage" class="text-center">Resim seçin veya sürükleyip bırakın</div>
                         <div v-else class="w-full h-full rounded-[6px] overflow-hidden">
@@ -110,7 +158,7 @@
                         </div>
                     </div>
                 </div>
-                <input type="text" id="bannerHeader" placeholder="Başlık giriniz..." class="border-[1px] rounded-[6px] px-[12px] py-[4px]">
+                <input type="text" id="bannerHeader" placeholder="Başlık giriniz..." class="border-[1px] rounded-[6px] px-[12px] py-[4px]" required>
                 <textarea name="description" id="bannerDescription" placeholder="Açıklama giriniz..." class="border-[1px] rounded-[6px] px-[12px] py-[4px]"></textarea>
                 <div class="flex justify-center items-center"><button type="button" @click="saveNewBanner" class="bg-second w-[120px] p-[4px] text-[17px] text-white rounded-[10px]">Kaydet</button></div>
             </form>
@@ -124,5 +172,12 @@
     }
     .bannerModal.open {
         display: flex;
+    }
+    .bannerCard:hover .deleteBanner {
+        opacity: 1;
+    }
+    .deleteBanner {
+        transition: all 0.3s ease;
+        opacity: 0;
     }
 </style>
