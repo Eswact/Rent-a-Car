2a53a87f7e6b93e7ae9bb748e17012beaa2fe5ab
Eren Sarıbay <eren99srb@gmail.com>
2024-01-16 10:27:15 +0300

    gitignore

--- Server/app/config/db-config.js
+++ Server/app/config/db-config.js
@@ -1,4 +1,5 @@
+require("dotenv").config();
 module.exports = {
     // url: "mongodb://127.0.0.1:27017/rentAcar"
-    url: "mongodb+srv://admin:bzVTEYhqpgmZWjwu@cluster0.lrhq0ak.mongodb.net/rentAcar?retryWrites=true&w=majority"
+    url: process.env.MONGODB_CONNECTION
 };
